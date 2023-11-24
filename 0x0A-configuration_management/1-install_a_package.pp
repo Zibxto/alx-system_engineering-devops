012529218f3598f269a6bcd8b7505f6f57fba0c4
zibxto <christianaziba@gmail.com>
2023-11-25 00:09:37 +0100

    downgraded werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,7 @@
 exec { 'pip install flask == 2.1.0':
     command => '/usr/bin/pip3 install flask==2.1.0'
 }
+
+exec { 'pip install werkzueg':
+    command => '/usr/bin/pip3 install werkzeug==2.1.1'
+}
