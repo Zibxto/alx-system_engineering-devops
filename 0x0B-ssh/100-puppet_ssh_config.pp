76805bb008adb93cf6e6916ddd9dea4dd5f83c9c
zibxto <christianaziba@gmail.com>
2023-11-27 03:40:44 +0100

    update file mode

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,11 +4,11 @@ file { '/home/zibxto/.ssh/ssh_config':
     ensure  => 'present',
     owner   => 'zibxto',
     group   => 'zibxto',
-    mode    => '0744',
+    mode    => '0600',
     content => 'Host test
-                    HostName 100.25.15.14
-                    User ubuntu
-                    PubkeyAuthentication yes
-                    IdentifyFile ~/.ssh/school
-                    PasswordAuthentication no'
+                HostName 100.25.15.14
+                User ubuntu
+                PubkeyAuthentication yes
+                IdentifyFile ~/.ssh/school
+                PasswordAuthentication no'
 }
