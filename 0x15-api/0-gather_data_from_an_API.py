e13efadee651bd77f3ed4176588419453f173557
zibxto <christianaziba@gmail.com>
2024-01-03 02:13:46 +0100

    add file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,14 +4,13 @@ Python script that, using this a REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
 import requests
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
     try:
-        if int(sys.argv[1]):
+        if int(argv[1]):
             r2 = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                              .format(sys.argv[1]))
+                              .format(argv[1]))
             res2 = r2.json()
             employee_name = res2.get("name")
 
@@ -21,7 +20,7 @@ if __name__ == "__main__":
             number_of_done_tasks = 0
             todo = []
             for item in res:
-                if item.get("userId") == int(sys.argv[1]):
+                if item.get("userId") == int(argv[1]):
                     total_number_of_tasks += 1
                     todo.append(item)
 
