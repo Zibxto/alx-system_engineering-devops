8aa5c1dfeadd26a83efe1e6e7935d2246d1ddaf2
zibxto <christianaziba@gmail.com>
2024-01-17 14:47:18 +0100

    update file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,15 +10,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Function that queries the Reddit API"""
-    res = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subreddit),
-                       headers={'User-Agent': 'CustomBot/1.0'})
+    try:
+        res = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers={'User-Agent': 'CustomBot/1.0'})
 
-    if res.status_code == requests.codes.ok:
-        res = res.json()
-        if 'subscribers' in res['data']:
-            return res['data']['subscribers']
+        if res.status_code == requests.codes.ok:
+            res = res.json()
+            if 'subscribers' in res['data']:
+                return res['data']['subscribers']
+            else:
+                return 0
         else:
             return 0
-    else:
+    except Exception:
         return 0
